730a653f5b1de77c52476b5cedaa7543db886439
rbplm <32923817+rbplm@users.noreply.github.com>
2017-11-17 13:44:28 +0200

    oma mõõtmetega korrutustabeli
    korrutustabeli loomise app

---
+++ 17.11 harjutused/mingi asi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mingi 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("sisesta korrutustabeli dimensioonid: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+            int input1 = Convert.ToInt32(Console.ReadLine());
+
+            int[,] array = new int[input, input1];
+
+            for (int i = 0; i < input; i++)
+            {
+                if (i == 0)
+                    continue;
+
+                for (int j = 0; j < input1; j++)
+                {
+                if (j == 0)
+                    continue;
+                    int multiplication = i * j;
+
+                    Console.Write(multiplication);
+
+                }
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
